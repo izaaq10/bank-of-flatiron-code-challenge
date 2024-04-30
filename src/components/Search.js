ac7b3460fe2104944356b6e96634e353cb709584
Izaaq <odongoizaaq@gmail.com>
2024-04-30 12:00:08 -0400

    Another commit

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React,{useState} from "react";
 
-function Search() {
+
+function Search({onSearch}) {
+  const[search, setSearch]= useState("")
+  function handleChange(e){
+    const searchText = e.target.value;
+    setSearch(searchText);
+    onSearch(searchText);
+  }
   return (
     <div className="ui large fluid icon input">
-      <input
+      <input value={search}
         type="text"
         placeholder="Search your Recent Transactions"
-        onChange={() => console.log("Searching...")}
+        onChange={(handleChange) => console.log("Searching...")}
       />
       <i className="circular search link icon"></i>
     </div>
